9744b259b7cc6330d36ea2a5f96f7c6dbb238fe5
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2022-11-08 10:21:47 +0100

    prettified and simplified tests

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -1,24 +1,13 @@
-import imp
 import logging
 from basht.metrics_storage import MetricsStorage
 from basht.resource_tracker import ResourceTracker
-from basht.results_tracker import ResultTracker
-from basht.workload.objective import Objective
-import torch
+
 from time import sleep
 
 
-def test_metrics(prometeus_url, resource_definition):
+def test_metrics(prometeus_url, prepared_objective):
     # setup
-    workload_def = resource_definition["workload"]
-    dl_framework = workload_def["dl_framework"]
-    model_cls = workload_def["model_cls"]
-    epochs = workload_def["epochs"]
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    task = workload_def["task"]
-    objective = Objective(
-            dl_framework=dl_framework, model_cls=model_cls, epochs=epochs, device=device,
-            task=task)
+    objective = prepared_objective
     metrics_storage = MetricsStorage()
     resourceTracker = ResourceTracker(prometheus_url=prometeus_url)
     try:
@@ -37,6 +26,7 @@ def test_metrics(prometeus_url, resource_definition):
         assert len(result["latency"]) > 0
         assert len(result["classification"]) > 0
         assert len(result["resources"]) > 0
+        assert isinstance(score, dict)
     except Exception as e:
         assert False, e
     finally:
--- test/test_yaml.py
+++ test/test_yaml.py
@@ -3,12 +3,10 @@ from basht.utils.yaml import YMLHandler
 
 
 def test_yaml():
-    resources = YMLHandler.load_yaml(path.join(path.dirname(__file__),"test.yaml"))
+    resources = YMLHandler.load_yaml(path.join(path.dirname(__file__), "test.yaml"))
     assert resources["deleteAfterRun"]
 
-    print(resources["hyperparameter"])
-
-    YMLHandler.as_yaml(path.join(path.dirname(__file__),"hyperparameter_space.yml"), resources["hyperparameter"])
-    params = YMLHandler.load_yaml(path.join(path.dirname(__file__),"hyperparameter_space.yml"))
+    YMLHandler.as_yaml(
+        path.join(path.dirname(__file__), "hyperparameter_space.yml"), resources["hyperparameter"])
+    params = YMLHandler.load_yaml(path.join(path.dirname(__file__), "hyperparameter_space.yml"))
     assert params == resources["hyperparameter"]
-
