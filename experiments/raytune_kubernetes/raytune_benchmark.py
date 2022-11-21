534b9b1f87d99b0e90175ae52abc97a7e2bc564e
basti.werner <werner@tu-berlin.de>
2022-11-21 14:08:39 +0100

    WIP!

--- experiments/raytune_kubernetes/raytune_benchmark.py
+++ experiments/raytune_kubernetes/raytune_benchmark.py
@@ -1,5 +1,6 @@
 from os import path
 import subprocess
+import time
 
 import ray
 from ray import tune
@@ -74,7 +75,7 @@ class RaytuneBenchmark(Benchmark):
             print("Namespace created. status='%s'" % str(resp))
         except ApiException as e:
             if self._is_create_conflict(e):
-                print("Deployment already exists")
+                print("Deployment (namespace) already exists")
             else:
                 raise e
         # deploy ray operator
@@ -85,8 +86,10 @@ class RaytuneBenchmark(Benchmark):
                 namespace=self.namespace, verbose=True
             )
         except FailToCreateError as e:
-            raise e
-
+            if self._is_create_conflict(e):
+                print("Deployment (operator) already exists")
+            else:
+                raise e
         # deploy ray cluster
         ray_cluster_definition = {
             "ray_worker_num": self.workerCount - 1,
@@ -103,17 +106,27 @@ class RaytuneBenchmark(Benchmark):
             ray_cluster_definition
         )
 
+        # wait for operator to be ready
+
+        time.sleep(30)
+
         ray_cluster_json_objects = next(ray_cluster_yml_objects)
-        try:
-            self.k8s_custom_objects_api.create_namespaced_custom_object(
+
+        self._k8s_create(
+            lambda: self.k8s_custom_objects_api.create_namespaced_custom_object(
                 group="cluster.ray.io",
                 version="v1",
                 namespace=self.namespace,
                 plural="rayclusters",
                 body=ray_cluster_json_objects
-            )
-        except FailToCreateError as e:
-            raise e
+            ),
+            lambda: self.k8s_custom_objects_api.delete_namespaced_custom_object(
+                    group="cluster.ray.io",
+                    namespace=self.namespace,
+                    name="ray-cluster",
+                )
+        )
+        
         with open("portforward_log.txt", 'w') as pf_log:
             self.portforward_proc = subprocess.Popen(
                 ["kubectl", "-n", self.namespace, "port-forward",
@@ -123,6 +136,18 @@ class RaytuneBenchmark(Benchmark):
         ray.init("ray://localhost:10001")
 
         self._deploy_watch()
+
+    def _k8s_create(self, create_fn, delete_fn):
+        try:
+            create_fn()
+        except Exception as e:
+            if self._is_create_conflict(e):
+                print("Deployment (cluster) already exists")
+                delete_fn()
+                time.sleep(5)
+                create_fn()
+            else:
+                raise e
 
     def setup(self):
         return
