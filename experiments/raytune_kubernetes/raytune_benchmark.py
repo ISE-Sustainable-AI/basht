c26941de408ce735a0f7f6624a01a9acc62df1cd
Michael Gebauer <gebauerm23@gmail.com>
2022-12-05 14:26:27 +0100

    reverted debugging code

--- experiments/raytune_kubernetes/raytune_benchmark.py
+++ experiments/raytune_kubernetes/raytune_benchmark.py
@@ -411,14 +411,14 @@ class RaytuneBenchmark(Benchmark):
 
 
 def main():
-    from urllib.request import urlopen
+    # from urllib.request import urlopen
 
     from basht.benchmark_runner import BenchmarkRunner
     # from basht.utils.yaml import YMLHandler
 
     resource_definition = YMLHandler.load_yaml(path.join(path.dirname(__file__), "resource_definition.yml"))
-    resource_definition["metricsIP"] = urlopen("https://checkip.amazonaws.com").read().decode("utf-8").strip()# resource_definition["metricsIP"]
-    resource_definition["kubernetesMasterIP"] = "192.168.49.2"
+    # resource_definition["metricsIP"] = urlopen("https://checkip.amazonaws.com").read().decode("utf-8").strip()# resource_definition["metricsIP"]
+    # resource_definition["kubernetesMasterIP"] = "192.168.49.2"
 
     runner = BenchmarkRunner(
         benchmark_cls=RaytuneBenchmark, resources=resource_definition)
