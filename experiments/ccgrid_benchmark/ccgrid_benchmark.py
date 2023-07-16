2a3271e74ba7251aa7f11cf81513f49f05c74b22
Michael Gebauer <gebauerm23@gmail.com>
2023-07-16 14:50:32 +0200

    added benchmark def

--- experiments/ccgrid_benchmark/ccgrid_benchmark.py
+++ experiments/ccgrid_benchmark/ccgrid_benchmark.py
@@ -38,8 +38,8 @@ class Experiment:
                 )
 
     def horizontal_exp(self):
-        start = 5
-        end = 6
+        start = 2
+        end = 2
 
         for worker_num in range(start, end+1):
             self.resource_definition.update(
@@ -95,7 +95,7 @@ class Experiment:
 
 
 if __name__ == "__main__":
-    for benchmark_cls in [OptunaKubernetesBenchmark, RaytuneBenchmark]:
+    for benchmark_cls in [RaytuneBenchmark]:
         if benchmark_cls is OptunaKubernetesBenchmark:
             experiment = Experiment(
                 benchmark_cls=benchmark_cls, name="ccgrid_run2", reps=2, dockertag="tawalaya/optuna-trial:latest")
